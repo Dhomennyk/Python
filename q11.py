58542a67159b96823265cb56559d5b0988d1cdfc
Dhomennyk <caped.2022123isinf0450@aluno.ifpi.edu.br>
2024-08-08 17:06:26 -0300

    Create q11.py

---
+++ q11.py
@@ -0,0 +1,9 @@
+a = input("Digite o primeiro número ")
+b = input("Digite o segundo número")
+if = a < b:
+    soma sum(range(a, b + 1) )
+    print(f"A soma dos números entreos números inteiros entre do intervalo ",)
+else:
+  print("ERRO!")
+  
+  
