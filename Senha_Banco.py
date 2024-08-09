f7cf5f04787b0c155dafb115337613d241e8c839
Dhomennyk <caped.2022123isinf0450@aluno.ifpi.edu.br>
2024-08-09 16:01:19 -0300

    Create Senha_Banco.py

---
+++ Senha_Banco.py
@@ -0,0 +1,11 @@
+usuário = input("Digite a senha do usuário:")
+senha = input("Digite a senha do usuário")
+if usuário == "admin" and senha == "123456"
+    print(f"Olá <Dhomennyk>, seja bem vindo ao nosso banco!")
+else:
+    print("Senha incorreta! Você ainda tem 2 tentativas.")
+  if ("usuário errar a senha pela segunda vez ")
+print("Senha incorreta! Você ainda tem 1 tentativa.")
+if ("usuário errar a senha pela terceira vez")
+print("Sua senha foi bloqueada! Por favor, dirija-se a um de nossos caixas.")
+print("fim do programa")
