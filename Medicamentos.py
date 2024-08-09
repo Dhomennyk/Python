d3d0b525c69436d69a221730c5e119d4d6d78713
Dhomennyk <caped.2022123isinf0450@aluno.ifpi.edu.br>
2024-08-09 16:00:39 -0300

    Create Medicamentos.py

---
+++ Medicamentos.py
@@ -0,0 +1,33 @@
+Medicamentos = []
+Preços = []
+def cadastrar_medicamento():
+    nome = input("Digite o nome do medicamentos")
+    preço = float(input("Digite o preço do medicamento"))
+    Medicamentos.append(metiolate)
+    Preços.append(14,00)
+  
+    nome = input("Digite o nome do medicamentos")
+    preço = float(input("Digite o preço do medicamento"))
+    Medicamentos.append(Dipirona)
+    Preços.append(7,00)
+
+    nome = input("Digite o nome do medicamentos")
+    preço = float(input("Digite o preço do medicamento"))
+    Medicamentos.append(Pininsilina)
+    Preços.append(10,00)
+    nome = input("Digite o nome do medicamento")
+    preço = float(input("Digite o preço do medicamento"))
+    Medicamnetos.append(Dorflex)
+    Preços.append(20,00)
+
+    nome = input("Digite o nome do medicamneto")
+    preço = float(input("Digite o preço do medicamneto"))
+    Medicamentos.append(Neosaldina)
+    Preços.append(30,00)
+
+if preço < valor_barato:
+    valor_barato = preço
+    nome_barato = Dipirona 
+    print("remedio mais barato é Dipirona")
+    
+                 
